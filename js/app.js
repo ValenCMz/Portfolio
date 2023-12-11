17058d5ef6d25b05d1dbacf06cef2c703e1bcb0f
Valentin <valentincaminos12345@gmail.com>
2023-12-11 15:27:18 -0300

    terminated structure

--- js/app.js
+++ js/app.js
@@ -17,17 +17,31 @@ let section_proy = document.querySelectorAll(".section__proyectos-proy_contImg")
 section_proy.forEach(section => {
 
     section.addEventListener("mouseover", () => {
-        // Si el mouse esta over lo scalo
         let img = section.querySelector(".section__proyectos-proy_img");
         img.style.transform = "scale(1.1)";
-        // img.style.transform = "translateY(-10px)";
     });
 
     section.addEventListener("mouseout", () => {
-        // Si el mouse esta fuera lo scalo
         let img = section.querySelector(".section__proyectos-proy_img");
-
         img.style.transform = "scale(1)";
     });
 });
 
+
+let section_proy_small = document.querySelectorAll(".section__proyectos-proy_small");
+section_proy_small.forEach(section => {
+    section.addEventListener("mouseover", () => {
+        let arrow = section.querySelector(".arrow_up");
+        arrow.style.opacity = "1";
+        section.style.transform = "translateY(-5px)";
+        let recuadro = section.querySelector(".section__proyectos-proy_small-item");
+        recuadro.style.boxShadow = "0px 0px 10px 0px rgba(0,0,0,0.75)";
+    });
+    section.addEventListener("mouseout", () => {
+        let arrow = section.querySelector(".arrow_up");
+        arrow.style.opacity = "0";
+        section.style.transform = "translateY(0px)";
+        let recuadro = section.querySelector(".section__proyectos-proy_small-item");
+        recuadro.style.boxShadow = "0px 0px 0px 0px rgba(0,0,0,0.75)";
+    });
+});
